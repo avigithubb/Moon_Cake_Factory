9064b4a88a1260eab9eb5141e2ca33c149aa98e8
Abhishek <avishek.npt@gmail.com>
2025-05-19 20:07:34 +0530

    debug False

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ env.read_env()
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'moon-cake-factory-4.onrender.com']
 
